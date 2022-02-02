e6af37f28a15637f6c77d888d80f0aad46ea7dbd
Anthony Barragan <anthony.bar.89@gmail.com>
2022-02-01 21:58:04 -0800

    updated form to take cuisine and lifestyle. Added some basic Bulma.

--- assests/js/index.js
+++ assests/js/index.js
@@ -6,10 +6,11 @@ const recipeFoodListEl = document.querySelector("#recipe_food");
 const recipeDrinkListEl = document.querySelector("#recipe_cocktails")
 
 
-const getRecipeFood = function (event) {
+const getRecipeTitleAndImage = function (event) {
     event.preventDefault();
-    const ingredient = document.querySelector("#choices_food").value;
-    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&includeIngredients=${ingredient}`
+    const cuisine = getCuisine();
+    const diet = getLifestyle();
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&cuisine=${cuisine}&diet=${diet}`
     fetch(url)
         .then(res => res.json())
         .then(data => {
@@ -21,27 +22,52 @@ const getRecipeFood = function (event) {
                 const imgEl = document.createElement("img")
                 imgEl.setAttribute("src", data.results[i].image);
                 recipeFoodListEl.appendChild(imgEl);
+                const recipeId = data.results[i].id 
             }
         });
 }
-const getRecipeCocktails = function (event) {
-    event.preventDefault();
-    const alcohol = document.querySelector("#choices_drinks").value;
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            for (let i = 0; i < data.drinks.length; i++) {
-                const listEl = document.createElement("p");
-                listEl.textContent = data.drinks[i].strDrink;
-                const imgEl = document.createElement("img")
-                imgEl.setAttribute("src", data.drinks[i].strDrinkThumb)
-                recipeDrinkListEl.appendChild(listEl);
-                recipeDrinkListEl.appendChild(imgEl);
-            }
-        })
+
+// const getRecipeCocktails = function (event) {
+//     event.preventDefault();
+//     const alcohol = document.querySelector("#choices_drinks").value;
+//     const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`;
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => {
+//             for (let i = 0; i < data.drinks.length; i++) {
+//                 const listEl = document.createElement("p");
+//                 listEl.textContent = data.drinks[i].strDrink;
+//                 const imgEl = document.createElement("img")
+//                 imgEl.setAttribute("src", data.drinks[i].strDrinkThumb)
+//                 recipeDrinkListEl.appendChild(listEl);
+//                 recipeDrinkListEl.appendChild(imgEl);
+//             }
+//         })
+// }
+
+// searchFoodBtn.addEventListener("click", getRecipeTitleAndImage);
+// searchDrinkBtn.addEventListener("click", getRecipeCocktails);
+
+function getCuisine () {
+    const foodItems = [];
+    const checkedItems = document.getElementsByClassName("cuisine");
+    for (let i = 0; i < checkedItems.length; i++) {
+        if (checkedItems[i].checked === true) {
+            foodItems.push(checkedItems[i].value);
+        }
+    }
+    const foodChoices = foodItems.join(",");
+    return foodChoices;
 }
-
-searchFoodBtn.addEventListener("click", getRecipeFood);
-searchDrinkBtn.addEventListener("click", getRecipeCocktails);
-
+function getLifestyle () {
+    let lifeStyleItems = [];
+    let checkedItems = document.getElementsByClassName("lifestyle");
+    for (let i = 0; i < checkedItems.length; i++) {
+        if (checkedItems[i].checked === true) {
+            lifeStyleItems.push(checkedItems[i].value);
+        }
+    }
+    const lifeStyle = lifeStyleItems.join(",");
+    return lifeStyle;
+}
+searchFoodBtn.addEventListener("click", getRecipeTitleAndImage)
