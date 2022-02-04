9fdae6025afef4cf8308b23916cf9da2152f6941
Anthony Barragan <anthony.bar.89@gmail.com>
2022-02-03 16:55:22 -0800

    isolated ingredient and measurements in drinks from API

--- assests/js/drinkIndex.js
+++ assests/js/drinkIndex.js
@@ -6,17 +6,20 @@ const resultsEl = document.querySelector("#results")
 const getRecipeCocktails = function (event) {
     event.preventDefault();
     const alcohol = getAlcohol();
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`;
-    fetch(url)
+    const urlByName = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`
+    fetch(urlByName)
         .then(res => res.json())
         .then(data => {
+            // cycle through data and display drinks as cards. 
             for (let i = 0; i < data.drinks.length; i++) {
-                // create message
+                
+                
+                // create Message
                 const messageEl = document.createElement("article");
                 messageEl.className = "message";
                 // create message header element and attach to message element
                 const messageHeaderEl = document.createElement("div");
-                messageHeaderEl.className ="message-header";
+                messageHeaderEl.className = "message-header";
                 // create title for message element and attach to header
                 const title = document.createElement("p");
                 title.textContent = data.drinks[i].strDrink;
@@ -25,18 +28,60 @@ const getRecipeCocktails = function (event) {
                 
                 // Add body to card
                 const messageBodyEl = document.createElement("div");
-                messageBodyEl.className = "message-body"
+                messageBodyEl.className = "message-body columns"
                 // create image
                 const imageEl = document.createElement("img");
                 imageEl.setAttribute("src", data.drinks[i].strDrinkThumb);
-                imageEl.className = "image"; 
+                imageEl.className = "image column";
                 messageBodyEl.appendChild(imageEl);
-                messageEl.appendChild(messageBodyEl);
                 
-                // append the whole thing
+                // get drink Id and store ingredients and measurements in an 2-d array
+                const drinkId = data.drinks[i].idDrink
+                const ingredientMeasurementList = getIngredientMeasurement(drinkId);
+                console.log(ingredientMeasurementList.length);
+                // for (let i = 0; i < ingredientMeasurementList[0].length; i++) {
+                //     const ingredient = ingredientMeasurementList[0][i];
+                //     const measurement = ingredientMeasurementList[1][i];
+                    
+                //     const ingredientMeasurementEl = document.createElement("p")
+                //     ingredientMeasurementEl.textContent = ingredient + " - " + measurement;
+                //     console.log(ingredientMeasurementEl)
+                //     ingredientMeasurementEl.className = "column";
+                //     messageBodyEl.appendChild(ingredientMeasurementEl);
+                // }
+                messageEl.appendChild(messageBodyEl);
+
+
+                // append the card to html whole thing
                 resultsEl.appendChild(messageEl);
             }
         })
+}
+
+const getIngredientMeasurement = function (id) {
+    let ingredient = [];
+    let measurement = [];
+    let ingredientMeasurement = [];
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const drinkObject = data.drinks[0]
+            for (const [key, value] of Object.entries(drinkObject)) {
+                if (key.includes("strIngredient") && value !== null && value !== "") {
+                    ingredient.push(value);
+                }
+            }
+            for (const [key, value] of Object.entries(drinkObject)) {
+                if (key.includes("strMeasure") && value !== null && value !== "") {
+                    measurement.push(value)
+                }
+            }
+            ingredientMeasurement.push(ingredient);
+            ingredientMeasurement.push(measurement);
+            console.log(ingredientMeasurement);
+        })
+            return ingredientMeasurement;
 }
 
 const getAlcohol = function () {
